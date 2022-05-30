5cea18a1af04fa50ecacdcd4a4adc651fb74ddb3
youjinjin <youjinjin@xiaoniuhy.com>
2022-05-30 19:50:05 +0800

    feat: init

--- src/views/page2/index.test.js
+++
@@ -1,7 +0,0 @@
-// import { mount } from '@vue/test-utils'
-// import Page2 from './index.vue'
-
-// test('测试1', () => {
-//   const wrapper = mount(Page2)
-//   expect(wrapper.text()).toContain('这里是Pinia返回的数据：')
-// })
